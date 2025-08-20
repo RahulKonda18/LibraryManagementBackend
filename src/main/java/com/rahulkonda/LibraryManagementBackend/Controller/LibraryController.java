45ab19751a06acaafecd8852289a6a55bb6bd508
rahulkonda18 <rahulmadhukonda@outlook.com>
2025-08-20 10:56:48 +0530

    configured supabase postgres db

--- src/main/java/com/rahulkonda/LibraryManagementBackend/Controller/LibraryController.java
+++ src/main/java/com/rahulkonda/LibraryManagementBackend/Controller/LibraryController.java
@@ -1,7 +1,13 @@
+
+package com.rahulkonda.LibraryManagementBackend.Controller;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
-
-public  class LibraryController{
-
+public class LibraryController {
+    
+    @GetMapping("/hi")
+    public String getDummy() {
+        return "Hello, World! From Spring Boot REST API!";
+    }
 }
