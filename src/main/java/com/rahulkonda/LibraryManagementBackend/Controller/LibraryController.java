05f2066cfacb536c50d64deffa75a414e9e54981
rahulkonda18 <rahulmadhukonda@outlook.com>
2025-08-20 11:00:18 +0530

    added health check controller

--- src/main/java/com/rahulkonda/LibraryManagementBackend/Controller/LibraryController.java
+++ src/main/java/com/rahulkonda/LibraryManagementBackend/Controller/LibraryController.java
@@ -2,8 +2,10 @@
 package com.rahulkonda.LibraryManagementBackend.Controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping("/api/")
 public class LibraryController {
     
     @GetMapping("/hi")
